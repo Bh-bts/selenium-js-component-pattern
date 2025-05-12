45b28d7b2c377bb583c7b19ff1f9aeb0e848a216
bh-bts <thumarbhavin.786@gmail.com>
2025-05-12 15:08:45 +0530

    Change to failed1

--- tests/specs/login.spec.js
+++ tests/specs/login.spec.js
@@ -51,7 +51,7 @@ describe(`Mypustak tests ${browser}`, function () {
         });
 
         afterEach(async function () {
-            if (this.currentTest.state === 'failed') {
+            if (this.currentTest.state === 'failed1') {
                 await ScreenshotUtil.capture(this.browser, this.currentTest.title);
             }
         });
